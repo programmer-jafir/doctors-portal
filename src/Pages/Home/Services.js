ad8987028e3a27c96bf72c758085d1e172bbd39c
programmer-jafir <jafiranjum2020@gmail.com>
2022-05-11 17:47:27 +0600

    navbar & banner added

---
+++ src/Pages/Home/Services.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Services = () => {
+    return (
+        <div className='my-28'>
+            <div className='text-center'>
+            <h1 className='text-primary  text-xl font-bold uppercase'>Our Services</h1>
+            <h2 className='text-4xl'>Services We Provide</h2>
+            </div>
+        </div>
+    );
+};
+
+export default Services;
