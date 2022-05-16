3838186c8b9908f236ea8de138ed97381848d3aa
programmer-jafir <jafiranjum2020@gmail.com>
2022-05-16 12:41:19 +0600

    firebase add & booking add

--- src/Pages/Appointment/AvailableAppointments.js
+++ src/Pages/Appointment/AvailableAppointments.js
@@ -1,18 +1,20 @@
 import { format } from 'date-fns';
 import React, { useEffect, useState } from 'react';
+import BookingModal from './BookingModal';
 import Service from './Service';
 
 const AvailableAppointments = ({date}) => {
     const [services, setServices] = useState([]);
+    const [treatment, setTreatment] = useState(null);
 
     useEffect( () =>{
-        fetch('services.json')
+        fetch('http://localhost:5000/service')
         .then(res => res.json())
         .then(date => setServices(date));
     }, [])
     return (
         <div>
-            <h4 className='text-xl text-secondary text-center'>
+            <h4 className='text-xl text-secondary text-center my-12'>
             Available Appointments on {format(date, 'PP')}
                 </h4>
             <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5'>
@@ -20,9 +22,15 @@ const AvailableAppointments = ({date}) => {
                     services.map(service=><Service
                     key={service._id}
                     service={service}
+                    setTreatment={setTreatment}
                     />)
                 }
             </div>
+            {treatment && <BookingModal 
+            date={date}
+             treatment={treatment}
+             setTreatment={setTreatment}
+             />}
         </div>
     );
 };
